0f870414ead48d5896957955d09a124f4a02fbe5
Nicolas Epstein <nilueps@gmail.com>
2021-12-02 19:17:45 -0500

    second

--- src/solutions/solution_02.rs
+++ src/solutions/solution_02.rs
@@ -1,12 +1,10 @@
 use crate::input;
 
 pub fn a(lines: input::InputLines) {
+    let instructions = parse_instructions(lines);
+
     let mut horizontal_position = 0;
     let mut depth = 0;
-    let instructions = lines.map(|s| {
-        let mut v = s.split(" ");
-        (v.next().unwrap(), v.next().unwrap().parse::<i32>().unwrap())
-    });
 
     for instruction in instructions {
         let dir = instruction.0;
@@ -23,19 +21,20 @@ pub fn a(lines: input::InputLines) {
 }
 
 pub fn b(lines: input::InputLines) {
+    let instructions = parse_instructions(lines);
+
     let mut horizontal_position = 0;
     let mut depth = 0;
     let mut aim = 0;
 
-    let instructions = parse_instructions(lines);
 
     for instruction in instructions {
         let dir = instruction.0;
         let val = instruction.1;
         match dir {
-            "forward" => horizontal_position += val,
-            "up" => depth -= val,
-            "down" => depth += val,
+            "forward" => { horizontal_position += val; depth += aim * val },
+            "up" => aim -= val,
+            "down" => aim += val,
             &_ => {}
         }
     }
@@ -43,7 +42,7 @@ pub fn b(lines: input::InputLines) {
     println!("{}", horizontal_position * depth);
 }
 
-type Instruction = (&str, i32);
+type Instruction<'a> = (&'a str, i32);
 
 fn parse_instructions(lines: input::InputLines) -> Vec<Instruction> {
     lines
